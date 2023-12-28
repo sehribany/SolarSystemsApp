2a0240ad2213a37f63da8e9af9f4cb98dbd5e382
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2023-12-28 15:07:05 +0300

    added Closures

---
+++ SolarSystems/Utilities/Closures/Closures.swift
@@ -0,0 +1,14 @@
+//
+//  Closures.swift
+//  SolarSystems
+//
+//  Created by Şehriban Yıldırım on 27.12.2023.
+//
+
+import Foundation
+
+public typealias VoidClosure      = (() -> Void)
+public typealias IntClosure       = ((Int) -> Void)
+public typealias BoolClosure      = ((Bool) -> Void)
+public typealias StringClosure    = ((String) -> Void)
+public typealias IndexPathClosure = ((IndexPath) -> Void)
