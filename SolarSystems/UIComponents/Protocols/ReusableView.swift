55875f99fc7ffeb64a3d9cd84fb5aa1004a2102f
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2023-12-28 15:07:35 +0300

    added ReusableView

---
+++ SolarSystems/UIComponents/Protocols/ReusableView.swift
@@ -0,0 +1,10 @@
+//
+//  ReusableView.swift
+//  SolarSystems
+//
+//  Created by Şehriban Yıldırım on 27.12.2023.
+//
+
+public protocol ReusableView: AnyObject {
+    static var identifier: String { get }
+}
