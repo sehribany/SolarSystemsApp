afabf1f16a6e91c799b236f162459191a35518ca
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-01-03 03:08:58 +0300

    added DefaultsKey+Extensions

---
+++ SolarSystems/Utilities/Extensions/DefaultsKey+Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  DefaultsKey+Extensions.swift
+//  SolarSystems
+//
+//  Created by Şehriban Yıldırım on 2.01.2024.
+//
+
+import MobilliumUserDefaults
+
+public extension DefaultsKey {
+    static let isIntroCompleted = Key<Bool>(key: "isIntroCompleted")
+}
