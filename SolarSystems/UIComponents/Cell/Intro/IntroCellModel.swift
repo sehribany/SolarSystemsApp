60a3a7fbd98e0dba632a163fac9422fb413db40b
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-01-04 14:11:09 +0300

    Added IntroCellModel

--- SolarSystems/UIComponents/Cell/Intro/IntroCellModel.swift
+++ SolarSystems/UIComponents/Cell/Intro/IntroCellModel.swift
@@ -7,6 +7,7 @@
 import UIKit
 
 public protocol IntroCellDataSource: AnyObject {
+    var image           : UIImage {get}
     var titleText       : String  {get}
     var descriptionText : String  {get}
 }
@@ -17,10 +18,12 @@ public protocol IntroCellProtocol: IntroCellDataSource, IntroCellEventSource {}
 
 public final class IntroCellModel: IntroCellProtocol {
     
+    public var image           : UIImage
     public var titleText       : String
     public var descriptionText : String
     
-    public init(titleText: String, descriptionText: String) {
+    public init(image: UIImage, titleText: String, descriptionText: String) {
+        self.image           = image
         self.titleText       = titleText
         self.descriptionText = descriptionText
     }
