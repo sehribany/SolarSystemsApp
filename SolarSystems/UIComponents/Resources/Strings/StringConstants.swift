1daf4b4e9c6a0e601fb4b4eed6fafda0bf3d7de1
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-01-07 01:06:47 +0300

    New Strings added.

--- SolarSystems/UIComponents/Resources/Strings/StringConstants.swift
+++ SolarSystems/UIComponents/Resources/Strings/StringConstants.swift
@@ -11,21 +11,76 @@ import Foundation
 // swiftlint:disable nesting type_body_length type_name vertical_whitespace_opening_braces
 public enum L10n {
   public enum Intro {
-    /// Ready to Explore Our Galaxy?
-    public static let firstTitle = L10n.tr("Intro", "Intro.firstTitle", fallback: "Ready to Explore Our Galaxy?")
-    /// Embark on a Journey Beyond Imagination!
-    public static let fourthTitle = L10n.tr("Intro", "Intro.fourthTitle", fallback: "Embark on a Journey Beyond Imagination!")
-    /// Intro.strings
-    ///   SolarSystems
-    /// 
-    ///   Created by Şehriban Yıldırım on 27.12.2023.
-    public static let next = L10n.tr("Intro", "Intro.Next", fallback: "Continue!")
-    /// Buckle Up for Interstellar Travel!
-    public static let secondTitle = L10n.tr("Intro", "Intro.secondTitle", fallback: "Buckle Up for Interstellar Travel!")
-    /// Get Started!
-    public static let start = L10n.tr("Intro", "Intro.Start", fallback: "Get Started!")
-    /// Unveiling the Gates to Infinite Space.
-    public static let thirdTitle = L10n.tr("Intro", "Intro.thirdTitle", fallback: "Unveiling the Gates to Infinite Space.")
+    public enum Intro {
+      /// Ready to Explore Our Galaxy?
+      public static let firstTitle = L10n.tr("Intro", "Intro.firstTitle", fallback: "Ready to Explore Our Galaxy?")
+      /// Embark on a Journey Beyond Imagination!
+      public static let fourthTitle = L10n.tr("Intro", "Intro.fourthTitle", fallback: "Embark on a Journey Beyond Imagination!")
+      /// Intro.strings
+      ///   SolarSystems
+      /// 
+      ///   Created by Şehriban Yıldırım on 27.12.2023.
+      public static let next = L10n.tr("Intro", "Intro.Next", fallback: "Continue!")
+      /// Buckle Up for Interstellar Travel!
+      public static let secondTitle = L10n.tr("Intro", "Intro.secondTitle", fallback: "Buckle Up for Interstellar Travel!")
+      /// Get Started!
+      public static let start = L10n.tr("Intro", "Intro.Start", fallback: "Get Started!")
+      /// Unveiling the Gates to Infinite Space.
+      public static let thirdTitle = L10n.tr("Intro", "Intro.thirdTitle", fallback: "Unveiling the Gates to Infinite Space.")
+    }
+  }
+  public enum Planets {
+    public enum Earth {
+      /// Earth
+      public static let name = L10n.tr("Planets", "Earth.Name", fallback: "Earth")
+      /// Venus, the Solar System's second planet, is known as the 'evening star' or 'morning star'. Its dense carbon dioxide-rich atmosphere maintains surface temperatures around 450°C. Despite surface volcanic traces, thick clouds hinder detailed observations. It also has a slow rotation and a smooth orbit.
+      public static let title = L10n.tr("Planets", "Earth.Title", fallback: "Venus, the Solar System's second planet, is known as the 'evening star' or 'morning star'. Its dense carbon dioxide-rich atmosphere maintains surface temperatures around 450°C. Despite surface volcanic traces, thick clouds hinder detailed observations. It also has a slow rotation and a smooth orbit.")
+    }
+    public enum Jupiter {
+      /// Jupiter
+      public static let name = L10n.tr("Planets", "Jupiter.Name", fallback: "Jupiter")
+      /// Venus, the Solar System's second planet, is known as the 'evening star' or 'morning star'. Its dense carbon dioxide-rich atmosphere maintains surface temperatures around 450°C. Despite surface volcanic traces, thick clouds hinder detailed observations. It also has a slow rotation and a smooth orbit.
+      public static let title = L10n.tr("Planets", "Jupiter.Title", fallback: "Venus, the Solar System's second planet, is known as the 'evening star' or 'morning star'. Its dense carbon dioxide-rich atmosphere maintains surface temperatures around 450°C. Despite surface volcanic traces, thick clouds hinder detailed observations. It also has a slow rotation and a smooth orbit.")
+    }
+    public enum Mars {
+      /// Mars
+      public static let name = L10n.tr("Planets", "Mars.Name", fallback: "Mars")
+      /// Venus, the Solar System's second planet, is known as the 'evening star' or 'morning star'. Its dense carbon dioxide-rich atmosphere maintains surface temperatures around 450°C. Despite surface volcanic traces, thick clouds hinder detailed observations. It also has a slow rotation and a smooth orbit.
+      public static let title = L10n.tr("Planets", "Mars.Title", fallback: "Venus, the Solar System's second planet, is known as the 'evening star' or 'morning star'. Its dense carbon dioxide-rich atmosphere maintains surface temperatures around 450°C. Despite surface volcanic traces, thick clouds hinder detailed observations. It also has a slow rotation and a smooth orbit.")
+    }
+    public enum Mercury {
+      /// Planets.strings
+      ///   SolarSystems
+      /// 
+      ///   Created by Şehriban Yıldırım on 6.01.2024.
+      public static let name = L10n.tr("Planets", "Mercury.Name", fallback: "Mercury")
+      /// Mercury is the smallest planet in the Solar System and the closest to the Sun. Its surface is filled with craters, and its temperatures vary greatly: It can reach up to 430°C during the day and drop to -180°C at night. It has almost no atmosphere, and its orbit is highly eccentric, sometimes very close and other times farther away from the Sun.
+      public static let title = L10n.tr("Planets", "Mercury.Title", fallback: "Mercury is the smallest planet in the Solar System and the closest to the Sun. Its surface is filled with craters, and its temperatures vary greatly: It can reach up to 430°C during the day and drop to -180°C at night. It has almost no atmosphere, and its orbit is highly eccentric, sometimes very close and other times farther away from the Sun.")
+    }
+    public enum Neptun {
+      /// Neptun
+      public static let name = L10n.tr("Planets", "Neptun.Name", fallback: "Neptun")
+      /// Venus, the Solar System's second planet, is known as the 'evening star' or 'morning star'. Its dense carbon dioxide-rich atmosphere maintains surface temperatures around 450°C. Despite surface volcanic traces, thick clouds hinder detailed observations. It also has a slow rotation and a smooth orbit.
+      public static let title = L10n.tr("Planets", "Neptun.Title", fallback: "Venus, the Solar System's second planet, is known as the 'evening star' or 'morning star'. Its dense carbon dioxide-rich atmosphere maintains surface temperatures around 450°C. Despite surface volcanic traces, thick clouds hinder detailed observations. It also has a slow rotation and a smooth orbit.")
+    }
+    public enum Saturn {
+      /// Saturn
+      public static let name = L10n.tr("Planets", "Saturn.Name", fallback: "Saturn")
+      /// Venus, the Solar System's second planet, is known as the 'evening star' or 'morning star'. Its dense carbon dioxide-rich atmosphere maintains surface temperatures around 450°C. Despite surface volcanic traces, thick clouds hinder detailed observations. It also has a slow rotation and a smooth orbit.
+      public static let title = L10n.tr("Planets", "Saturn.Title", fallback: "Venus, the Solar System's second planet, is known as the 'evening star' or 'morning star'. Its dense carbon dioxide-rich atmosphere maintains surface temperatures around 450°C. Despite surface volcanic traces, thick clouds hinder detailed observations. It also has a slow rotation and a smooth orbit.")
+    }
+    public enum Uranus {
+      /// Uranus
+      public static let name = L10n.tr("Planets", "Uranus.Name", fallback: "Uranus")
+      /// Venus, the Solar System's second planet, is known as the 'evening star' or 'morning star'. Its dense carbon dioxide-rich atmosphere maintains surface temperatures around 450°C. Despite surface volcanic traces, thick clouds hinder detailed observations. It also has a slow rotation and a smooth orbit.
+      public static let title = L10n.tr("Planets", "Uranus.Title", fallback: "Venus, the Solar System's second planet, is known as the 'evening star' or 'morning star'. Its dense carbon dioxide-rich atmosphere maintains surface temperatures around 450°C. Despite surface volcanic traces, thick clouds hinder detailed observations. It also has a slow rotation and a smooth orbit.")
+    }
+    public enum Venus {
+      /// Venus
+      public static let name = L10n.tr("Planets", "Venus.Name", fallback: "Venus")
+      /// Venus, the Solar System's second planet, is known as the 'evening star' or 'morning star'. Its dense carbon dioxide-rich atmosphere maintains surface temperatures around 450°C. Despite surface volcanic traces, thick clouds hinder detailed observations. It also has a slow rotation and a smooth orbit.
+      public static let title = L10n.tr("Planets", "Venus.Title", fallback: "Venus, the Solar System's second planet, is known as the 'evening star' or 'morning star'. Its dense carbon dioxide-rich atmosphere maintains surface temperatures around 450°C. Despite surface volcanic traces, thick clouds hinder detailed observations. It also has a slow rotation and a smooth orbit.")
+    }
   }
 }
 // swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
