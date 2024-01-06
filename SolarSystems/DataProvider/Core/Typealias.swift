e596fe2e86c87e174ef7aa62ed8856fed9aa860e
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-01-07 01:01:58 +0300

    typealias added.

---
+++ SolarSystems/DataProvider/Core/Typealias.swift
@@ -0,0 +1,9 @@
+//
+//  Typealias.swift
+//  SolarSystems
+//
+//  Created by Şehriban Yıldırım on 5.01.2024.
+//
+
+public typealias RequestParameters = [String: Any]
+public typealias RequestHeaders    = [String: String]
