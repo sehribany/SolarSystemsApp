aa731dacfcf427cffa608234259ca1e98a27a159
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-01-07 01:01:27 +0300

    RequestEncoding and RequestMethod added.

---
+++ SolarSystems/DataProvider/Core/RequestEncoding.swift
@@ -0,0 +1,11 @@
+//
+//  RequestEncoding.swift
+//  SolarSystems
+//
+//  Created by Şehriban Yıldırım on 5.01.2024.
+//
+
+public enum RequestEncoding {
+    case url
+    case json
+}
---
+++ SolarSystems/DataProvider/Core/RequestMethod.swift
@@ -0,0 +1,27 @@
+//
+//  RequestMethod.swift
+//  SolarSystems
+//
+//  Created by Şehriban Yıldırım on 5.01.2024.
+//
+
+public enum RequestMethod {
+    /// `CONNECT` method.
+    case connect
+    /// `DELETE` method.
+    case delete
+    /// `GET` method.
+    case get
+    /// `HEAD` method.
+    case head
+    /// `OPTIONS` method.
+    case options
+    /// `PATCH` method.
+    case patch
+    /// `POST` method.
+    case post
+    /// `PUT` method.
+    case put
+    /// `TRACE` method.
+    case trace
+}
