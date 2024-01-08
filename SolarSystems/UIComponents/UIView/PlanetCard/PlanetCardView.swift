440ec64a502a092c210ef3d568d7c5289a5ee6dc
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-01-08 23:54:55 +0300

    PlanetCardView Created.

---
+++ SolarSystems/UIComponents/UIView/PlanetCard/PlanetCardView.swift
@@ -0,0 +1,70 @@
+//
+//  PlanetCardView.swift
+//  SolarSystems
+//
+//  Created by Şehriban Yıldırım on 6.01.2024.
+//
+
+import UIKit
+
+class PlanetCardView: UIView {
+        
+    public let titleLabel: UILabel = {
+        let label           = UILabel()
+        label.font          = UIFont.font(.montserratExtraBold, size: .custom(size: 26))
+        label.textAlignment = .left
+        return label
+    }()
+    
+    public var descriptionLabel: UILabel = {
+        let label                       = UILabel()
+        label.font                      = UIFont.font(.roundedRegular, size: .medium)
+        label.textAlignment             = .left
+        label.textColor                 = .appGray
+        label.numberOfLines             = 0
+        label.adjustsFontSizeToFitWidth = true
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubViews()
+        configureContents()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        addSubViews()
+        configureContents()
+    }
+}
+
+//MARK: - UILayout
+extension PlanetCardView{
+    
+    private func addSubViews(){
+        addTitleLabel()
+        addDescriptionLabel()
+    }
+    
+    private func addTitleLabel(){
+        addSubview(titleLabel)
+        titleLabel.leftToSuperview().constant = 20
+        titleLabel.topToSuperview().constant  = 54
+    }
+
+    private func addDescriptionLabel(){
+        addSubview(descriptionLabel)
+        descriptionLabel.leftToSuperview().constant           = 20
+        descriptionLabel.trailingToSuperview().constant       = -20
+        descriptionLabel.topToBottom(of: titleLabel).constant = 6
+    }
+}
+
+// MARK: - Configure and Set Localize
+extension PlanetCardView {
+    private func configureContents(){
+        backgroundColor    = Asset.Colors.appWhite.color
+        layer.cornerRadius = 30
+    }
+}
