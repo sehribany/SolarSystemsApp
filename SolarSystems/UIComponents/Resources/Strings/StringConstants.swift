6f857c3cc889033d7d7d11cfe28a5c1e4376c03e
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-01-08 23:55:11 +0300

    New String Added.

--- SolarSystems/UIComponents/Resources/Strings/StringConstants.swift
+++ SolarSystems/UIComponents/Resources/Strings/StringConstants.swift
@@ -33,20 +33,20 @@ public enum L10n {
     public enum Earth {
       /// Earth
       public static let name = L10n.tr("Planets", "Earth.Name", fallback: "Earth")
-      /// Venus, the Solar System's second planet, is known as the 'evening star' or 'morning star'. Its dense carbon dioxide-rich atmosphere maintains surface temperatures around 450°C. Despite surface volcanic traces, thick clouds hinder detailed observations. It also has a slow rotation and a smooth orbit.
-      public static let title = L10n.tr("Planets", "Earth.Title", fallback: "Venus, the Solar System's second planet, is known as the 'evening star' or 'morning star'. Its dense carbon dioxide-rich atmosphere maintains surface temperatures around 450°C. Despite surface volcanic traces, thick clouds hinder detailed observations. It also has a slow rotation and a smooth orbit.")
+      /// Earth is the third planet in the Solar System and is known as the 'blue planet.' It is the only known celestial body that hosts and supports various forms of life. It features a moderate climate with continents and oceans on its surface. Its atmosphere primarily consists of nitrogen and oxygen, providing suitable temperatures for the development of life. Earth's rotation and orbit allow for the creation of diverse climates and seasons.
+      public static let title = L10n.tr("Planets", "Earth.Title", fallback: "Earth is the third planet in the Solar System and is known as the 'blue planet.' It is the only known celestial body that hosts and supports various forms of life. It features a moderate climate with continents and oceans on its surface. Its atmosphere primarily consists of nitrogen and oxygen, providing suitable temperatures for the development of life. Earth's rotation and orbit allow for the creation of diverse climates and seasons.")
     }
     public enum Jupiter {
       /// Jupiter
       public static let name = L10n.tr("Planets", "Jupiter.Name", fallback: "Jupiter")
-      /// Venus, the Solar System's second planet, is known as the 'evening star' or 'morning star'. Its dense carbon dioxide-rich atmosphere maintains surface temperatures around 450°C. Despite surface volcanic traces, thick clouds hinder detailed observations. It also has a slow rotation and a smooth orbit.
-      public static let title = L10n.tr("Planets", "Jupiter.Title", fallback: "Venus, the Solar System's second planet, is known as the 'evening star' or 'morning star'. Its dense carbon dioxide-rich atmosphere maintains surface temperatures around 450°C. Despite surface volcanic traces, thick clouds hinder detailed observations. It also has a slow rotation and a smooth orbit.")
+      /// Jupiter is the largest planet in the Solar System and ranks fifth in position. Known as the 'Gas Giant,' it is composed primarily of gases like hydrogen and helium. It features prominent characteristics such as a massive atmospheric storm called the Great Red Spot. It has numerous moons, with Ganymede being recognized as the largest natural satellite in the Solar System. Jupiter also boasts a strong magnetic field and a distinct ring system.
+      public static let title = L10n.tr("Planets", "Jupiter.Title", fallback: "Jupiter is the largest planet in the Solar System and ranks fifth in position. Known as the 'Gas Giant,' it is composed primarily of gases like hydrogen and helium. It features prominent characteristics such as a massive atmospheric storm called the Great Red Spot. It has numerous moons, with Ganymede being recognized as the largest natural satellite in the Solar System. Jupiter also boasts a strong magnetic field and a distinct ring system.")
     }
     public enum Mars {
       /// Mars
       public static let name = L10n.tr("Planets", "Mars.Name", fallback: "Mars")
-      /// Venus, the Solar System's second planet, is known as the 'evening star' or 'morning star'. Its dense carbon dioxide-rich atmosphere maintains surface temperatures around 450°C. Despite surface volcanic traces, thick clouds hinder detailed observations. It also has a slow rotation and a smooth orbit.
-      public static let title = L10n.tr("Planets", "Mars.Title", fallback: "Venus, the Solar System's second planet, is known as the 'evening star' or 'morning star'. Its dense carbon dioxide-rich atmosphere maintains surface temperatures around 450°C. Despite surface volcanic traces, thick clouds hinder detailed observations. It also has a slow rotation and a smooth orbit.")
+      /// Mars is the fourth planet in the Solar System and is known as the 'Red Planet.' It stands out with desert-like reddish soil and icy polar regions. It has a thin atmosphere with a high concentration of carbon dioxide. Its surface features massive mountains and a giant volcano. There is evidence indicating the presence of past water, fueling speculation about the possibility of past life. Mars' orbit and tilt contribute to the changing seasons.
+      public static let title = L10n.tr("Planets", "Mars.Title", fallback: "Mars is the fourth planet in the Solar System and is known as the 'Red Planet.' It stands out with desert-like reddish soil and icy polar regions. It has a thin atmosphere with a high concentration of carbon dioxide. Its surface features massive mountains and a giant volcano. There is evidence indicating the presence of past water, fueling speculation about the possibility of past life. Mars' orbit and tilt contribute to the changing seasons.")
     }
     public enum Mercury {
       /// Planets.strings
@@ -60,26 +60,39 @@ public enum L10n {
     public enum Neptun {
       /// Neptun
       public static let name = L10n.tr("Planets", "Neptun.Name", fallback: "Neptun")
-      /// Venus, the Solar System's second planet, is known as the 'evening star' or 'morning star'. Its dense carbon dioxide-rich atmosphere maintains surface temperatures around 450°C. Despite surface volcanic traces, thick clouds hinder detailed observations. It also has a slow rotation and a smooth orbit.
-      public static let title = L10n.tr("Planets", "Neptun.Title", fallback: "Venus, the Solar System's second planet, is known as the 'evening star' or 'morning star'. Its dense carbon dioxide-rich atmosphere maintains surface temperatures around 450°C. Despite surface volcanic traces, thick clouds hinder detailed observations. It also has a slow rotation and a smooth orbit.")
+      /// Neptune is the eighth and farthest planet in the Solar System. It is known as a gas giant. Its blue-colored atmosphere is composed of methane. The strongest winds are observed here. A giant dark spot known as the Great Dark Spot is a prominent feature. Triton, an unusually retrograde moon, is Neptune's largest moon. It has rings, but they are faint and difficult to observe.
+      public static let title = L10n.tr("Planets", "Neptun.Title", fallback: "Neptune is the eighth and farthest planet in the Solar System. It is known as a gas giant. Its blue-colored atmosphere is composed of methane. The strongest winds are observed here. A giant dark spot known as the Great Dark Spot is a prominent feature. Triton, an unusually retrograde moon, is Neptune's largest moon. It has rings, but they are faint and difficult to observe.")
     }
     public enum Saturn {
       /// Saturn
       public static let name = L10n.tr("Planets", "Saturn.Name", fallback: "Saturn")
-      /// Venus, the Solar System's second planet, is known as the 'evening star' or 'morning star'. Its dense carbon dioxide-rich atmosphere maintains surface temperatures around 450°C. Despite surface volcanic traces, thick clouds hinder detailed observations. It also has a slow rotation and a smooth orbit.
-      public static let title = L10n.tr("Planets", "Saturn.Title", fallback: "Venus, the Solar System's second planet, is known as the 'evening star' or 'morning star'. Its dense carbon dioxide-rich atmosphere maintains surface temperatures around 450°C. Despite surface volcanic traces, thick clouds hinder detailed observations. It also has a slow rotation and a smooth orbit.")
+      /// Saturn is the sixth planet in the Solar System and is renowned for its dazzling rings. It's a gas giant primarily composed of hydrogen and helium. Its rings are made up of ice and rock particles. With numerous moons, the largest being Titan, it possesses an atmosphere with a different chemical composition than Earth's. Saturn, along with its unique rings, stands out as a planet that captivates space enthusiasts' interest.
+      public static let title = L10n.tr("Planets", "Saturn.Title", fallback: "Saturn is the sixth planet in the Solar System and is renowned for its dazzling rings. It's a gas giant primarily composed of hydrogen and helium. Its rings are made up of ice and rock particles. With numerous moons, the largest being Titan, it possesses an atmosphere with a different chemical composition than Earth's. Saturn, along with its unique rings, stands out as a planet that captivates space enthusiasts' interest.")
     }
     public enum Uranus {
       /// Uranus
       public static let name = L10n.tr("Planets", "Uranus.Name", fallback: "Uranus")
-      /// Venus, the Solar System's second planet, is known as the 'evening star' or 'morning star'. Its dense carbon dioxide-rich atmosphere maintains surface temperatures around 450°C. Despite surface volcanic traces, thick clouds hinder detailed observations. It also has a slow rotation and a smooth orbit.
-      public static let title = L10n.tr("Planets", "Uranus.Title", fallback: "Venus, the Solar System's second planet, is known as the 'evening star' or 'morning star'. Its dense carbon dioxide-rich atmosphere maintains surface temperatures around 450°C. Despite surface volcanic traces, thick clouds hinder detailed observations. It also has a slow rotation and a smooth orbit.")
+      /// Uranus is the seventh planet in the Solar System and stands out with its tilted orbit, rotating horizontally unlike other planets. Its surface clouds and atmospheric features, composed of elements like hydrogen, helium, and methane, make exploration and observation challenging. With several major moons and a ring system, Uranus is a planet that captures the interest of space enthusiasts.
+      public static let title = L10n.tr("Planets", "Uranus.Title", fallback: "Uranus is the seventh planet in the Solar System and stands out with its tilted orbit, rotating horizontally unlike other planets. Its surface clouds and atmospheric features, composed of elements like hydrogen, helium, and methane, make exploration and observation challenging. With several major moons and a ring system, Uranus is a planet that captures the interest of space enthusiasts.")
     }
     public enum Venus {
       /// Venus
       public static let name = L10n.tr("Planets", "Venus.Name", fallback: "Venus")
-      /// Venus, the Solar System's second planet, is known as the 'evening star' or 'morning star'. Its dense carbon dioxide-rich atmosphere maintains surface temperatures around 450°C. Despite surface volcanic traces, thick clouds hinder detailed observations. It also has a slow rotation and a smooth orbit.
-      public static let title = L10n.tr("Planets", "Venus.Title", fallback: "Venus, the Solar System's second planet, is known as the 'evening star' or 'morning star'. Its dense carbon dioxide-rich atmosphere maintains surface temperatures around 450°C. Despite surface volcanic traces, thick clouds hinder detailed observations. It also has a slow rotation and a smooth orbit.")
+      /// Venus is the second planet in the Solar System and is known as the 'evening star' or the 'morning star.' It is one of the hottest planets, with surface temperatures averaging around 450°C due to its dense carbon dioxide atmosphere. Despite traces of volcanic activity on its surface, detailed observations are hindered by a thick layer of clouds. Its rotation is slow, and its orbit is relatively smooth.
+      public static let title = L10n.tr("Planets", "Venus.Title", fallback: "Venus is the second planet in the Solar System and is known as the 'evening star' or the 'morning star.' It is one of the hottest planets, with surface temperatures averaging around 450°C due to its dense carbon dioxide atmosphere. Despite traces of volcanic activity on its surface, detailed observations are hindered by a thick layer of clouds. Its rotation is slow, and its orbit is relatively smooth.")
+    }
+  }
+  public enum Segments {
+    public enum Segments {
+      /// Segments.strings
+      ///   SolarSystems
+      /// 
+      ///   Created by Şehriban Yıldırım on 8.01.2024.
+      public static let firstTitle = L10n.tr("Segments", "Segments.firstTitle", fallback: "Planet")
+      /// Dwarf Planet
+      public static let secondTitle = L10n.tr("Segments", "Segments.secondTitle", fallback: "Dwarf Planet")
+      /// Celestial Bodie
+      public static let thirdTitle = L10n.tr("Segments", "Segments.thirdTitle", fallback: "Celestial Bodie")
     }
   }
 }
