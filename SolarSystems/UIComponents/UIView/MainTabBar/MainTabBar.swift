4e43148d5cb94336c5cb79b3ac3a432b2a779171
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-01-11 00:29:28 +0300

    MainTabBar updated.

--- SolarSystems/UIComponents/UIView/MainTabBar/MainTabBar.swift
+++ SolarSystems/UIComponents/UIView/MainTabBar/MainTabBar.swift
@@ -12,7 +12,7 @@ class MainTabBar: UITabBar {
     private var shapeLayer: CALayer?
     
     override func draw(_ rect: CGRect) {
-        let ovalRect = CGRect(x: 20, y: -15, width: bounds.width - 40, height: 70)
+        let ovalRect = CGRect(x: 20, y: -15, width: bounds.width - 40, height: 65)
         let maskPath = UIBezierPath(roundedRect: ovalRect, cornerRadius: 35).cgPath
 
         let maskLayer  = CAShapeLayer()
@@ -22,7 +22,7 @@ class MainTabBar: UITabBar {
         let shapeLayer         = CAShapeLayer()
         shapeLayer.path        = maskPath
         shapeLayer.strokeColor = Asset.Colors.appWhite.color.cgColor
-        shapeLayer.fillColor   = Asset.Colors.appWhite.color.cgColor
+        shapeLayer.fillColor   = UIColor.clear.cgColor
 
 
         if let sublayers = layer.sublayers, sublayers.contains(shapeLayer) {
