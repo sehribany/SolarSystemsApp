1d4fcc712cce7bd0ce521901263d5d83cf41d414
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-01-20 22:36:04 +0300

    backGround update

--- SolarSystems/Scenes/CelestialBodies/CelestialBodiesViewController.swift
+++ SolarSystems/Scenes/CelestialBodies/CelestialBodiesViewController.swift
@@ -9,15 +9,20 @@ import UIKit
 
 final class CelestialBodiesViewController: BaseViewController<CelestialBodiesViewModel> {
     
+    private let backGround: UIImageView = {
+        let imageView         = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         backgroundDraw()
     }
     
     private func backgroundDraw(){
-        let gradientLayer    = CAGradientLayer()
-        gradientLayer.frame  = view.bounds
-        gradientLayer.colors = [UIColor.appYellow1.cgColor, UIColor.appPurple1.cgColor]
-        view.layer.insertSublayer(gradientLayer, at: 0)
+        view.addSubview(backGround)
+        backGround.image = Asset.Images.homeScreen.image
+        backGround.edgesToSuperview()
     }
 }
