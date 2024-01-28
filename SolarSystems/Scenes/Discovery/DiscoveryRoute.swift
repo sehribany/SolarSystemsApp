cdcc905bcea99676804bfbbf6f7b9e4b00c93baf
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-01-28 15:01:28 +0300

    Discovery Scenes Created.

--- SolarSystems/Scenes/Discovery/DiscoveryRoute.swift
+++ SolarSystems/Scenes/Discovery/DiscoveryRoute.swift
@@ -5,4 +5,23 @@
 //  Created by Şehriban Yıldırım on 4.01.2024.
 //
 
-import Foundation
+protocol DiscoveryRoute{
+    func openPlanetsDiscovery()
+}
+
+extension DiscoveryRoute where Self: RouterProtocol{
+    
+    func openPlanetDiscovery(){
+        let router               = DiscoveryRouter()
+        let viewModel            = DiscoveryViewModel(router: router)
+        let viewController       = DiscoveryViewController(viewModel: viewModel)
+        let navigationController = MainNavigationController(rootViewController: viewController)
+
+        let transition           = PushTransition()
+        router.viewController    = viewController
+        router.openTransition    = transition
+        
+        open(navigationController, transition: transition)
+    }
+}
+
